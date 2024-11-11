98a4c2238be6ff1018df01ad936a91dfee63971a
Ismatulla <ismatilloqoyliyev@gmail.com>
2024-11-11 11:33:49 +0500

    footer

--- 14-dars/script.js
+++ 14-dars/script.js
@@ -1,1 +1,10 @@
-console.log('script');
+function countUp(n) {
+  let example = 0;
+  for (let i = 1; i <= n; i++) {
+    example += i;
+    console.log(example, 'in loop');
+  }
+  console.log(example, 'out loop');
+  return example;
+}
+countUp(6);
