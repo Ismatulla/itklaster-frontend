3e654b4281f1e0fe11dde13e620dd05b3e98cc0f
Ismatulla <ismatilloqoyliyev@gmail.com>
2024-11-26 12:16:50 +0500

    responsivenes

--- 14-dars/script.js
+++ 14-dars/script.js
@@ -1,10 +1,19 @@
-function countUp(n) {
-  let example = 0;
-  for (let i = 1; i <= n; i++) {
-    example += i;
-    console.log(example, 'in loop');
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let freqCounter = {};
+  for (let char of str1) {
+    freqCounter[char] = (freqCounter[char] || 0) + 1;
+    console.log(freqCounter);
   }
-  console.log(example, 'out loop');
-  return example;
+  for (let char of str2) {
+    if (!freqCounter[char]) return false;
+    freqCounter[char] -= 1;
+    console.log(freqCounter);
+  }
+
+  return true;
 }
-countUp(6);
+
+isAnagram('David', 'divad');
+isAnagram('Cake', 'Pake');
